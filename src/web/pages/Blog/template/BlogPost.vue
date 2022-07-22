3fdc318710f8104af572468fe803b5e483fdde97
Stephen Li <stephenli.ca@gmail.com>
2022-07-22 19:24:56 -0400

    Add blog post template

---
+++ src/web/pages/Blog/template/BlogPost.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import dayjs from 'dayjs'
+import { defineComponent } from 'vue'
+
+export const TITLE = ''
+export const CREATED_AT = dayjs.utc('1970-01-01').unix()
+
+export default defineComponent({
+    setup() {
+        return {
+            TITLE,
+            CREATED_AT,
+        }
+    },
+})
+</script>
+
+<template>
+    <BlogPost
+        :title="TITLE"
+        :created-at="CREATED_AT"
+    >
+        <!--  -->
+    </BlogPost>
+</template>
